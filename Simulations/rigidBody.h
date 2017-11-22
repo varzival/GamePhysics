d3d2716bf9770cd3c79ea71b2a40393cd68280d8
Sebastian Di Luzio <sebidiluzio@gmail.com>
2017-11-22 13:39:52 +0100

    added matrices

--- Simulations/rigidBody.h
+++ Simulations/rigidBody.h
@@ -9,11 +9,17 @@ public:
 	~rigidBody();
 
 	Vec3 pos;
+	matrix4x4<float> transMat();
 	Vec3 scale;//h,d,w
+	matrix4x4<float> scaleMat();
 	Quaternion<float> rot;
+	matrix4x4<float> rotMat();
 	float mass;
 	matrix4x4<float> inertia();
-	matrix4x4<float> master;
+	matrix4x4<float> master();
+
+	//TODO
+	//liste aus kräften mit richtungsvektor und ortsvektor wo sie wirkt
 
 
 };
